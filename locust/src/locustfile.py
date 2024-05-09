7030e8c1a22a731ed97d528ae5e69e31123bb1e2
filbary <panbaryczka123@gmail.com>
2024-05-09 17:33:17 +0200

    t

--- locust/src/locustfile.py
+++ locust/src/locustfile.py
@@ -1,9 +1,13 @@
+"""Module to perform load testing with Locust."""
+
 from locust import HttpUser, task, between
 
 class QuickstartUser(HttpUser):
-    wait_time = between(1, 2)
+    """A user class for quick start load testing scenarios."""
+    
+    wait_time = between(1, 2)  # time between tasks
 
     @task
     def hello_world(self):
+        """Accesses the convert endpoint to simulate a user interaction."""
         self.client.get("/convert/43")
-
