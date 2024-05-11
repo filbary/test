7d39984484d2fdd41dee1ae17cd77527ce909824
filbary <panbaryczka123@gmail.com>
2024-05-12 00:46:19 +0200

    s

--- src/locust/locustfiles/webapp.py
+++ src/locust/locustfiles/webapp.py
@@ -1,8 +1,11 @@
+"""Module to perform load testing with Locust."""
 from locust import HttpUser, task, between
 
 class WebappUser(HttpUser):
+    """A user class for quick start load testing scenarios."""
     wait_time = between(1, 2)
 
     @task
     def convert(self):
+        """Accesses the convert endpoint to simulate a user interaction."""
         self.client.post("/login", json={"fahrenheit":42})
